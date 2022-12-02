2797e062f0f9a0fdda1603ce3eaf1c3f7a5dcec2
Christian Martinez <christian@mixdlabs.com>
2022-12-02 15:49:17 -0700

    Removed duplicates, removed some cringe ones :)

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express()
 const port = process.env.PORT || 3000
 const { pickuplines } = require('./pickuplines')
 
+// Get rid of duplicate pickup lines
+//const duplicates = pickuplines.filter((x, i) => pickuplines.indexOf(x) !== i)
+//
+//for (const x of duplicates) {
+//  console.log(`Duplicate: ${x} at indexes ${
+//    [...Array(pickuplines.length).keys()].filter(i => pickuplines[i] === x)}`);
+//}
+
 app.get('/', (req, res) => {
   const randomNum = Math.floor(Math.random() * pickuplines.length)
   const pickupLine = pickuplines[randomNum]
